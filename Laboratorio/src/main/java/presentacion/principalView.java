5ce8971ccd4963413207ee85ccd5a56392fd8a8e
geritolivera <geronimo.olivera.1995@gmail.com>
2020-08-31 00:19:11 -0300

    edEXT Logo

--- Laboratorio/src/main/java/presentacion/principalView.java
+++ Laboratorio/src/main/java/presentacion/principalView.java
@@ -39,7 +39,7 @@ public class principalView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame = new JFrame("edEXT");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
