e36c1275bec077818b22086e1d48166ab6706d89
rubbu <csuarez2211@gmail.com>
2020-09-05 20:50:07 -0300

    Cambiadas funciones en los controladores para tirar mas excepciones. Cambiadas algunas funciones. Eliminadas otras funciones que no se usan.

--- Laboratorio/src/main/java/interfaces/IcontroladorUsuario.java
+++ Laboratorio/src/main/java/interfaces/IcontroladorUsuario.java
@@ -17,25 +17,16 @@ public interface IcontroladorUsuario {
 	//2 - Consulta de Usuario
 	public ArrayList<String> listarUsuarios();
 	public List<DTUsuario> listarDTUsuarios();
-	public void verInfoUsuario(String nickname);
+	public void verInfoUsuario(String nickname) throws UsuarioExcepcion;
 	
 	//3 - Modificar Datos de Usuario
 	//Se utiliza la misma funcion listarUsuario
-	public DTUsuario seleccionarUsuario(String nickname);
-	public void nuevosDatos(String nickname, String nombre, String apellido, Date fechaNaci);
-	
-	//6 - Alta de Edicion de Curso
-	public void ingresarDocente(String nomDoc);
-	
-	//8 - Inscripcion a Edicion de Curso
-	public void seleccionarEstudiante(String nickname);
-	public void confirmarInscripcion(String nickname, Date fechaInsc);
-	public void cancelarInscripcion();
-	public void agregarEdicionUsuario(EdicionCurso edV);
+	//public DTUsuario seleccionarUsuario(String nickname);
+	public void nuevosDatos(String nickname, String nombre, String apellido, Date fechaNaci) throws UsuarioExcepcion;
 	
 	//12 - Alta de Instituto
 	public void AltaInstituto(String nombre) throws InstitutoExcepcion;
 	
 	//Funciones auxiliares
-	public String[] listarInstituto(); //Lista los usuarios
+	public ArrayList<String> listarInstituto();
 }
