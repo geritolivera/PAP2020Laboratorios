d24840485009a322aca2076a239a497796df6b97
geritolivera <geronimo.olivera.1995@gmail.com>
2020-09-07 14:58:03 -0300

    fix

--- Laboratorio/src/main/java/interfaces/IcontroladorUsuario.java
+++ Laboratorio/src/main/java/interfaces/IcontroladorUsuario.java
@@ -7,28 +7,35 @@ import exepciones.*;
 
 public interface IcontroladorUsuario {
 	
+	
+	/*-------------------------------------------------------------------------------------------------------------*/
 	//1 - Alta Usuario
 	public void AltaUsuario(String nickname, String nombre, String apellido, String correo, Date fechaNac, String instituto) throws UsuarioExcepcion;
-	//public void AltaUsuario(DTUsuario usuario)throws UsuarioRepetidoExcepcion;
 	
+	
+	/*-------------------------------------------------------------------------------------------------------------*/
 	//2 - Consulta de Usuario
 	public ArrayList<String> listarUsuarios();
-	public List<DTUsuario> listarDTUsuarios();
 	public void verInfoUsuario(String nickname) throws UsuarioExcepcion;
 	
+	
+	/*-------------------------------------------------------------------------------------------------------------*/
 	//3 - Modificar Datos de Usuario
-	//Se utiliza la misma funcion listarUsuario
-	//public DTUsuario seleccionarUsuario(String nickname);
 	public void nuevosDatos(String nickname, String nombre, String apellido, Date fechaNaci);
 	
 	
+	/*-------------------------------------------------------------------------------------------------------------*/
 	//12 - Alta de Instituto
 	public void AltaInstituto(String nombre) throws InstitutoExcepcion;
 	
+	
+	/*-------------------------------------------------------------------------------------------------------------*/
 	//Funciones auxiliares
 
-	public String[] listarInstituto();
+	public List<DTUsuario> listarDTUsuarios();
+	
+	public String[] listarInstituto(); //Lista los nombres de los institutos
 
-	public String[] listarEstudiantesAux();
+	public String[] listarEstudiantesAux(); //Lista los nicknames de los estudiantes
 	
 }
