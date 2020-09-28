156144e77b543d471b09e895b7a2c701e78ac94f
nicode97 <nicomiller97@gmail.com>
2020-09-27 21:14:10 -0300

    Creacion proycecto web

---
+++ edExtWeb/installDependency.mk
@@ -0,0 +1,7 @@
+mvn install:install-file \
+   -Dfile=edExtHibernate.jar \
+   -DgroupId=com.codebind \
+   -DartifactId=Laboratorio \
+   -Dversion=0.0.1-SNAPSHOT \
+   -Dpackaging=jar \
+   -DgeneratePom=true
